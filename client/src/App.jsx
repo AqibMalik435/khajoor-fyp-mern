2d37aa8ee55a9c49363047fd27c05e9509c64a6a
Yasir Nawaz <y451rmahar@gmail.com>
2024-03-18 09:43:39 +0500

    :fire: removed old Admin UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -72,6 +72,7 @@ export default function App() {
           </Route>
             
           <Route path="*" element={<NotFoundPage />} />
+          <Route path="admin" element={Admin} />
         </Routes>
 
       </UserContext.Provider>
